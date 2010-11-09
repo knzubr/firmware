2f4b2618fb4adf8802d5d84b0567bbff1bb0031d
adam <adam@b4570558-3e8d-400d-a0f3-90fa2602697e>
2010-11-09 13:47:27 +0000

    Uporządkowany opis protokołu działającego na mogitrali RS 485

--- FreeRtos/freeRtos/Lib/include/protocol1.h
+++ FreeRtos/freeRtos/Lib/include/protocol1.h
@@ -1,19 +1,49 @@
-// Addressing
+#ifndef PROTOCOL1_H
+#define PROTOCOL1_H 1
+
+// Addressing devices on RS 485 bus
 #define FIRST_ROLLER_DRIVER_ADDR 1
 #define LAST_ROLLER_DRIVER_ADDR  31
+
 #define FIRST_LIGHT_DRIVER_ADDR  32
 #define LAST_LIGHT_DRIVER_ADDR   63
+
 #define FIRST_SENSOR_ADDR        64
 #define LAST_SENSOR_ADDR         95
 
-// General message
-#define HDR_LEN         4
-//  + header                             + data                + CRC           +
+// *********************************** General message format *****************************************************
+#define HDR_LEN                  4
+#define SYNC                     0x5A
+
+//  + header (4 bytes)                   + data                + CRC           +
 //  +------+----------+---------+--------+-------+     +-------+-------+-------+
 //  | SYNC | op. code | address | length | data1 | ... | dataN | CrcHi | CrcLo |
+//  | 0x5A |          |         |        |       |     |       |       |       |
 //  +------+----------+---------+--------+-------+     +-------+-------+-------+
 //
-// hello response
+
+// *********************************** Ping message  **************************************************************
+#define rPING             0x80
+
+
+// *********************************** Hello message **************************************************************
+#define rHELLO            0x82
+
+// hello: master->slave
+//  +------+------+---------+--------+-------+-------+
+//  | SYNC | type | address | length | CrcHi | CrcLo |
+//  | 0x5A | 0x82 |         |    0   |       |       |
+//  +------+------+---------+--------+-------+-------+
+
+
+// hello response: slave->master
+//  +------+------+---------+--------+--------+--------+-------+-------+-------+-------+-------+-------+-------+
+//  | SYNC | type | address | length | data1  | data2  | data3 | data4 | data5 | data6 | data7 | CrcHi | CrcLo |
+//  | 0x5A | 0x82 |    0    |    7   | state1 | state2 |version|version|version|version|version|       |       |
+//  |      |      |         |        |        |        |   v   |       |  .    |       |       |       |       |
+//  +------+------+---------+--------+--------+--------+-------+-------+-------+-------+-------+-------+-------+
+
+//offsets of hello response data field 
 #define HELLO_RESP_STATE1  0
 #define HELLO_RESP_STATE2  1
 #define HELLO_RESP_VER     2
@@ -21,33 +51,30 @@
 #define HELLO_RESP_LEN     2+HELLO_RESP_VER_LEN
 
 
-//  +------+----------+---------+--------+--------+--------+-------+-------+-------+-------+-------+-------+-------+
-//  | SYNC | rHELLO   | address | length | data1  | data2  | data3 | data4 | data5 | data6 | data7 | CrcHi | CrcLo |
-//  |      |          |    0    |    7   | state1 | state2 |version|version|version|version|version|       |       |
-//  |      |          |         |        |        |        |   v   |       |  .    |       |       |       |       |
-//  +------+----------+---------+--------+--------+--------+-------+-------+-------+-------+-------+-------+-------+
-// mode:
-//  b - bootloader
-//  r - roller driver
-//  s - sensor driver
-// Version 5 bytes in ascii code
 
-
-#ifndef PROTOCOL1_H
-#define PROTOCOL1_H 1
-
-
-
-#define SYNC              0x5A
-#define rOpuscRolete1     0x10
-#define rOpuscRolete2     0x11
-#define rPodniesRolete1   0x20
-#define rPodniesRolete2   0x21
-#define rZatrzymajRolete1 0x30
-#define rZatrzymajRolete2 0x31
-#define rPING             0x80
+// *********************************** Flash firmware message  *****************************************************
 #define rFLASH            0x81
-#define rHELLO            0x82
 
+
+// *********************************** Roller blind 1 down *********************************************************
+#define rOpuscRolete1     0x10
+
+// *********************************** Roller blind 1 down *********************************************************
+#define rOpuscRolete2     0x11
+
+// *********************************** Roller blind 1 up ***********************************************************
+#define rPodniesRolete1   0x20
+
+// *********************************** Roller blind 2 up ***********************************************************
+#define rPodniesRolete2   0x21
+
+// *********************************** Roller 1 stop     ***********************************************************
+#define rZatrzymajRolete1 0x30
+
+// *********************************** Roller 2 stop     ***********************************************************
+#define rZatrzymajRolete2 0x31
+
+// *********************************** Unknown message  ************************************************************
 #define rNIEZNANY         0xFF
+
 #endif /* PROTOCOL1_H */
