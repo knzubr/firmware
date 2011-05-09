c0675e9c20d8af0ed0205560d44c1b218a3c43ff
adam <32864941+adamkaliszan@users.noreply.github.com>
2011-05-09 09:03:38 +0000

    Szablony na zajęcia

---
+++ Lab/Coroutines/hardware.h
@@ -0,0 +1,92 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#include <avr/io.h>
+#include "main.h"
+#include "../../freeRtos/Lib/include/protocol1.h"
+
+#define TxStart() (PORTD |=  0x0C)
+#define TxStop()  (PORTD &=  0xF3)
+
+extern uint8_t address;
+extern char bHelloResp[];
+
+/**
+ * Herdware initialization
+ */
+void hardwareInit(void);
+
+void ledOn(uint8_t lenNo);
+void ledOff(uint8_t ledNo);
+void ledToggle(uint8_t ledNo);
+char readKey(uint8_t keyNo);
+
+/**
+ * Switch on (enable) Led 1
+ */
+void led1on(void);
+/**
+ * Switch off (disable) Led 1
+ */
+void led1off(void);
+/**
+ * Toggle (change state) Led 1
+ */
+void led1toggle(void);
+/**
+ * Read key #1
+ */
+char readKey1(void);
+
+/**
+ * Switch on (enable) Led 1
+ */
+void led2on(void);
+/**
+ * Switch off (disable) Led 1
+ */
+void led2off(void);
+/**
+ * Toggle (change state) Led 1
+ */
+void led2toggle(void);
+/**
+ * Read key #1
+ */
+char readKey2(void);
+
+/**
+ * Switch on (enable) Led 1
+ */
+void led3on(void);
+/**
+ * Switch off (disable) Led 1
+ */
+void led3off(void);
+/**
+ * Toggle (change state) Led 1
+ */
+void led3toggle(void);
+/**
+ * Read key #1
+ */
+char readKey3(void);
+
+/**
+ * Switch on (enable) Led 1
+ */
+void led4on(void);
+/**
+ * Switch off (disable) Led 1
+ */
+void led4off(void);
+/**
+ * Toggle (change state) Led 1
+ */
+void led4toggle(void);
+/**
+ * Read key #1
+ */
+char readKey4(void);
+
+#endif
