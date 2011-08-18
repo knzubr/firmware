9ec3cca8828c3fc685bc3a93f434de5a97fc7d11
jakubm <jakubm@b4570558-3e8d-400d-a0f3-90fa2602697e>
2011-08-18 22:03:06 +0000

    check main.c  *   xTaskCreate(encTask,        NULL /*ENC    */, STACK_SIZE_ENC,       (void *)CLIStateSerialUsb->myStdInOut, 0, &xHandleEnc);    //&CLIStateSerialUsb  - to jest błędnie

--- FreeRtos/projektyAdam/SterownikGlowny/main.c
+++ FreeRtos/projektyAdam/SterownikGlowny/main.c
@@ -108,7 +108,7 @@ portSHORT main( void )
   initQueueStream(&udpStream, &udpBuffers, udpSocket->Rx, udpSocket->Tx);
   VtyInit(CLIStateSerialUdp, &udpStream);
   
-  xTaskCreate(encTask,        NULL /*"ENC"    */, STACK_SIZE_ENC,       (void *)&CLIStateSerialUsb->myStdInOut, 0, &xHandleEnc);
+  xTaskCreate(encTask,        NULL /*"ENC"    */, STACK_SIZE_ENC,       (void *)CLIStateSerialUsb->myStdInOut, 0, &xHandleEnc);
   xTaskCreate(vTaskVTYusb,    NULL /*"VTY"    */, STACK_SIZE_VTY,       (void *)(CLIStateSerialUsb),            1, &xHandleVTY_USB);
   xTaskCreate(vTaskVTYsocket, NULL /*"VTY"    */, STACK_SIZE_VTY,       (void *)(CLIStateSerialUdp),            1, &xHandleVTY_UDP);
   xTaskCreate(sensorsTask,    NULL /*"Sensors"*/, STACK_SIZE_SENSORS,   NULL,                                   1, &xHandleSensors);
