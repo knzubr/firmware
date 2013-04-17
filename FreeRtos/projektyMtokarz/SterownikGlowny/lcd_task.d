28913a82af73628156451637b2d475b52be2b4e0
adam <adam@b4570558-3e8d-400d-a0f3-90fa2602697e>
2013-04-17 16:20:35 +0000

    Wyrzucenie śmieci

--- FreeRtos/projektyMtokarz/SterownikGlowny/lcd_task.d
+++
@@ -1,7 +0,0 @@
-lcd_task.o lcd_task.d : lcd_task.c lcd_task.h \
- ../../freeRtos/Source/include/FreeRTOS.h \
- ../../freeRtos/Source/include/projdefs.h FreeRTOSConfig.h \
- ../../freeRtos/Source/include/portable.h \
- ../../freeRtos/Source/include/../../portable/GCC/ATMega323/portmacro.h \
- ../../freeRtos/Source/include/mpu_wrappers.h \
- ../../freeRtos/Source/include/queue.h ../../freeRtos/Lib/include/lcd.h
